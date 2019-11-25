5035a05b259ef250c3c588c5de98178c30b84fff
Iyah Nuestro <iyah@educredit.ph>
2019-11-26 00:54:42 +0800

    Initial Commit

---
+++ components/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sidebarContainer">
+    <div class="province" v-for="province in provinces" @click="$emit('province-selected', province)">
+      {{ province }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      provinces: ["Abra", "Agusan del Norte", "Agusan del Sur", "Aklan", "Albay", "Angeles", "Antique", "Apayao", "Aurora", "Baguio", "Basilan", "Bataan", "Batanes", "Batangas", "Benguet", "Biliran", "Bohol", "Bukidnon", "Bulacan", "Cagayan", "Camarines Norte", "Camarines Sur", "Camiguin", "Capiz", "Catanduanes", "Cavite", "Cebu", "Compostela Valley", "Cotabato", "Dagupan", "Davao Occidental", "Davao Oriental", "Davao del Norte", "Davao del Sur", "Dinagat Islands", "Eastern Samar", "Guimaras", "Ifugao", "Ilocos Norte", "Ilocos Sur", "Iloilo", "Isabela", "Kalinga", "La Union", "Laguna", "Lanao del Norte", "Lanao del Sur", "Leyte", "Lucena", "Maguindanao", "Marinduque", "Masbate", "Metro Manila", "Mindoro Occidental", "Mindoro Oriental", "Misamis Occidental", "Misamis Oriental", "Mountain Province", "Naga", "Negros Occidental", "Negros Oriental", "Northern Samar", "Nueva Ecija", "Nueva Vizcaya", "Olongapo", "Palawan", "Pampanga", "Pangasinan", "Puerto Princesa", "Quezon", "Quirino", "Rizal", "Romblon", "Samar", "Santiago", "Sarangani", "Siquijor", "Sorsogon", "South Cotabato", "Southern Leyte", "Sultan Kudarat", "Sulu", "Surigao del Norte", "Surigao del Sur", "Tarlac", "Tawi-Tawi", "Zambales", "Zamboanga", "Zamboanga del Norte", "Zamboanga del Sur"]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebarContainer {
+  // background: rgb(55,55,55);
+  // background: linear-gradient(180deg, rgba(55,55,55,1) 0%, rgba(11,11,11,1) 100%);
+  background-color: rgba($color: #000000, $alpha: 0.25);
+  color: white;
+  height: 100vh;
+  overflow-y: scroll;
+  position: fixed;
+  // width: 200px;
+  .province {
+    &:hover {
+      // transform: scale(1.5);
+      // transform-origin: center left;
+    }
+    padding: 5px 12px;
+    transition: 0.25s;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="page-container">
+    <navbar></navbar>
+    <div class="palette-container container">
+      <div class="row">
+        <div class="col palette" 
+          v-for="province in provinceData.find(x => x.id === this.getProvince()).palette"
+          v-bind:style="{backgroundColor: province.hex}">
+          <div class="color-name">
+            {{ province.name }}
+          </div>
+          <div class="color-hex">
+            {{ province.hex }}
+          </div>
+        </div>
+      </div>
+      <div class="row province-row">
+        <div class="col province-col">
+          <div class="province-name">
+            {{ this.getProvince() }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from '~/components/navbar'
+
+export default {
+  data() {
+    return {
+      currentProvince: 'Abra',
+      provinceData: [
+        {
+          'id': 'Abra',
+          'palette': [
+            {
+              'name': 'Midnight Blue',
+              'hex': '#1C4C64',
+            },
+            {
+              'name': 'Light Sea Green',
+              'hex': '#2D9999',
+            },
+            {
+              'name': 'Light Steel Blue',
+              'hex': '#D2E2D6',
+            },
+            {
+              'name': 'Dark Sea Green',
+              'hex': '#8FB6A4',
+            },
+            {
+              'name': 'Dark Olive Green',
+              'hex': '#31662B',
+            }
+          ],
+        }
+      ]
+    }
+  },
+  components: {
+    navbar
+  },
+  computed: {
+    selectedProvince() {
+      return currentProvince;
+    }
+  },
+  methods: {
+    getProvince() {
+      return 'Abra';
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.page-container {
+  .palette-container {
+    margin: 0;
+    max-width: none;
+    overflow: hidden;
+    width: 100%;
+    .palette {
+      height: 100vh;
+      text-align: center;
+      .color-name, .color-hex {
+        background-color: rgba($color: #000000, $alpha: 0.25);
+        border-radius: 5px;
+        color: white;
+        margin: 12px;
+        padding: 12px;
+      }
+    }
+    .province-row {
+      align-items: center;   
+      display: flex;            
+      flex-direction: column;   
+      justify-content: center; 
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      //transform: translateY(-100%);
+      .province-col {
+        .province-name {
+          color: rgba($color: #fff, $alpha: 0.50);
+          font-family: 'Barabara';
+          font-size: 100px;
+          text-align: center;
+        }
+      }
+    }
+  }
+}
+</style>
